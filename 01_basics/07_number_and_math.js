3540a58b99debbac3005ebfdf00ebffc2435e34f
yash jain <141411221+yash-jn-026@users.noreply.github.com>
2024-01-16 13:06:43 +0000

    push

---
+++ 01_basics/07_number_and_math.js
@@ -0,0 +1,36 @@
+/*
+let score = 400
+console.log(score)
+
+const balance = new Number(100)
+console.log(balance)
+
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+
+const unotherNum = 3399.09090
+console.log(unotherNum.toPrecision(4))
+
+const unotherNum2 = 1000000
+console.log(unotherNum2.toLocaleString('en-IN'))
+
+*/
+
+// ++++++++++++++++++++++ maths +++++++++++++++++
+
+console.log(Math)
+console.log(Math.abs(-3))
+console.log(Math.round(878.90))
+console.log(Math.ceil(878.90))
+console.log(Math.min(8,7,8,9,0))
+console.log(Math.max(8,7,8,9,0))
+
+console.log(Math.random())
+console.log(Math.floor(Math.random()*10) +1 )
+
+const min =10 
+const max = 20  
+
+console.log(Math.floor(Math.random() * (max - min + 1 )) + min)
