5e531b52d3c61e9d6c4989c0dc45c0da8e4680dd
yash jain <141411221+yash-jn-026@users.noreply.github.com>
2024-01-18 11:58:00 +0000

    push

---
+++ 05_itreation/two.js
@@ -0,0 +1,14 @@
+let index = 1
+
+while (index<=10) {
+    console.log(`value of index is ${index}`);
+    index = index+2
+    
+}
+
+let myArray= ["ironman" , 'spiderman', "batman"]
+let arr = 0
+while(arr<myArray.length){
+    console.log(`value is ${myArray[arr]}`);
+    arr =arr + 1
+}
